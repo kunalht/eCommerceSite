fbcbd28e89962bb8a7a400cff02174d0e3cd5585
kunalht <kunalht1@gmail.com>
2017-06-30 03:11:28 +0530

    login change

--- middleware/index.js
+++ middleware/index.js
@@ -21,38 +21,38 @@ middlewareObj.getRegister = function (req, res) {
 }
 
 
-middlewareObj.login = function (req, res) {
-    passport.authenticate("local-login", {
-        successRedirect: "/"
-    })(req, res, function () {
-        c.query("select * from user where email=:email",
-            { email: req.body.email },
-            function (err, foundUser) {
-                if (err) {
-                    console.log(err)
-                } else {
-                    if (!foundUser.length) {
-                        console.log("Wrong username")
-                    } else {
-                        if (bcrypt.compareSync(req.body.password,foundUser[0].password)) {
-                            console.log("correct")
-                            req.logIn(req.body.email,function(err){
-                                if(err){
-                                    console.log(err)
-                                    console.log("1")
-                                }else{
-                                    console.log("here")
-                                    res.redirect("/")
-                                }
-                            })
-                        } else {
-                            console.log("Wrong")
-                        }
-                    }
-                }
-            })
-    })
-}
+// middlewareObj.login = function (req, res) {
+//     passport.authenticate("local-login", {
+//         successRedirect: "/"
+//     })(req, res, function () {
+//         c.query("select * from user where email=:email",
+//             { email: req.body.email },
+//             function (err, foundUser) {
+//                 if (err) {
+//                     console.log(err)
+//                 } else {
+//                     if (!foundUser.length) {
+//                         console.log("Wrong username")
+//                     } else {
+//                         if (bcrypt.compareSync(req.body.password,foundUser[0].password)) {
+//                             console.log("correct")
+//                             req.logIn(req.body.email,function(err){
+//                                 if(err){
+//                                     console.log(err)
+//                                     console.log("1")
+//                                 }else{
+//                                     console.log("here")
+//                                     res.redirect("/")
+//                                 }
+//                             })
+//                         } else {
+//                             console.log("Wrong")
+//                         }
+//                     }
+//                 }
+//             })
+//     })
+// }
 // middlewareObj.login = function (req, res) {
 //     passport.authenticate('local-login', {
 //         successRedirect: "/",
